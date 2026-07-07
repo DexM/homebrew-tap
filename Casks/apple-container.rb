a2368d10218af6b625df0ccb5c3e069223d3c969
Kiril Piroženko <kiril@pirozenko.com>
2026-07-07 17:21:41 +0300

    Cask for Apple Container

---
+++ Casks/apple-container.rb
@@ -0,0 +1,42 @@
+cask "apple-container" do
+  version "1.1.0"
+  sha256 "0ca1c42a2269c2557efb1d82b1b38ac553e6a3a3da1b1179c439bcee1e7d6714"
+
+  url "https://github.com/apple/container/releases/download/#{version}/container-#{version}-installer-signed.pkg"
+  name "Apple Container"
+  desc "Create and run Linux containers using lightweight virtual machines"
+  homepage "https://github.com/apple/container"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on arch: :arm64
+  depends_on macos: :tahoe
+
+  pkg "container-#{version}-installer-signed.pkg"
+
+  generate_completions_from_executable "/usr/local/bin/container", "--generate-completion-script"
+
+  uninstall_preflight do
+    container_bin="/usr/local/bin/container"
+    next unless File.executable?(container_bin)
+
+    system_command container_bin,
+                   args: ["system", "stop"]
+  end
+
+  uninstall script: {
+    executable: "/usr/local/bin/uninstall-container.sh",
+    args:       ["-k"],
+    sudo:       true,
+  }
+
+  zap script: {
+        executable:   "defaults",
+        args:         ["delete", "com.apple.container.defaults"],
+        must_succeed: false,
+      },
+      trash:  "~/Library/Application Support/com.apple.container"
+end
--- README.md
+++ README.md
@@ -9,4 +9,5 @@ brew tap dexm/tap
 
 |Cask|Software|Comments|
 |-|-|-|
+|[`apple-container`](Casks/apple-container.rb)|[Apple Container](https://github.com/apple/container)|[Homebrew `container` Formula](https://github.com/Homebrew/homebrew-core/blob/main/Formula/c/container.rb) builds `container` from source and does not use provided installer. This Cask uses installer instead.|
 |[`docker-desktop`](Casks/docker-desktop.rb)|[Docker Desktop](https://www.docker.com/products/docker-desktop)|Same as [official Homebrew Cask](https://github.com/Homebrew/homebrew-cask/blob/main/Casks/d/docker-desktop.rb), but does not install symlinks automatically (Docker Desktop will do that itself on the first launch).|
