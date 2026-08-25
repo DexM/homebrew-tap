625a5206bb203e6ec10587d6e1d99ce1b49867e1
Kiril Piroženko <kiril@pirozenko.com>
2026-08-25 08:03:42 +0300

    Docker Desktop version bump

--- Casks/docker-desktop-clean.rb
+++ Casks/docker-desktop-clean.rb
@@ -1,9 +1,9 @@
 cask "docker-desktop-clean" do
   arch arm: "arm64", intel: "amd64"
 
-  version "4.86.0,236216"
-  sha256 arm:   "4b662c088e9cb4dfcfb59a64c6ea9ff39063af303a660c604e6f4a5eec5c5fe4",
-         intel: "3acbd44ef3e2f95deab09fbddac82b977af453f5762986baed4bd47151ae348b"
+  version "4.88.0,237115"
+  sha256 arm:   "1651abab9cda5b8088e02740815e38a112140e304b87dd682509ba1bf7f8b036",
+         intel: "e33e07833476741da8d8b1e022301f34276a0b7113c7f13cd41b3a6fd40fdbe7"
 
   on_intel do
     binary "#{appdir}/Docker.app/Contents/Resources/bin/com.docker.hyperkit",
